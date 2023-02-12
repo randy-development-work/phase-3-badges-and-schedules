fef41d469b5b5c5e19858a35db09e7f7e82bd340
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-02-12 11:57:55 +0300

    Completed assignment.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.collect {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(array)
+    array.map.with_index(1) {|name, index| "Hello, #{name}! You'll be assigned to room #{index}!"}
+end
+
+def printer(array)
+    batch_badge_creator(array).each{|badge| puts badge}
+    
+    assign_rooms(array).each{|assignment| puts assignment}
+end
+
+puts printer(["Arel", "Carol"])
